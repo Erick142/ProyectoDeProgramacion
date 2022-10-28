e40ae48a27532c6a21e8609cb75a826d004f0b75
Jorge <j.cisterna02@ufromail.cl>
2022-10-27 22:11:30 -0300

    3

--- src/main/java/proyect/programacion/service/ServicioPedido.java
+++ src/main/java/proyect/programacion/service/ServicioPedido.java
@@ -1,17 +1,9 @@
 package proyect.programacion.service;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import proyect.programacion.model.Pedido;
 import proyect.programacion.repository.RepoPedido;
-
-import javax.persistence.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public abstract class ServicioPedido implements RepoPedido{
---
+++ src/main/java/proyect/programacion/service/ServicioUsuario.java
@@ -0,0 +1,19 @@
+package proyect.programacion.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import proyect.programacion.model.Pedido;
+import proyect.programacion.model.Usuario;
+import proyect.programacion.repository.RepoPedido;
+import proyect.programacion.repository.RepoUsuario;
+
+@Service
+public abstract class ServicioUsuario implements RepoUsuario {
+    @Autowired
+    private RepoUsuario repoUsuario;
+
+    public Iterable<Usuario> findAllUsuarios() {
+        Iterable<Usuario> listadoUsuarios = repoUsuario.findAll();
+        return listadoUsuarios;
+    }
+}
